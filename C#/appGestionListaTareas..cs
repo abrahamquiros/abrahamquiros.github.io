9d3c0085829684da0beb08cd065980ad9e5d6377
abrahamquiros <97398804+abrahamquiros@users.noreply.github.com>
2024-09-17 22:33:49 +0200

    Add files via upload

---
+++ C#/appGestionListaTareas..cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eje5
+{
+    internal class Program
+    {
+        public struct Tarea
+        {
+            public string nombre;
+            public string descripcion;
+            public bool completada;
+        }
+
+        static List<Tarea> lista = new List<Tarea>();
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("1. Añadir tarea");
+                Console.WriteLine("2. Eliminar tarea");             
+                Console.WriteLine("3. Mostrar lista de tareas");
+                Console.WriteLine("4. Modificar tarea");
+                Console.WriteLine("5. Marcar tarea como completada");
+                Console.WriteLine("6. Salir");
+                Console.WriteLine("Selecciona una opción:");
+                int option;
+                if (!int.TryParse(Console.ReadLine(), out option))
+                {
+                    Console.WriteLine("Opción no válida. Introduce un número.");
+                    continue;
+                }
+                switch (option)
+                {
+                    case 1:
+                        AddTarea();
+                        break;
+                    case 2:
+                        BorraTarea();
+                        break;
+                    case 3:
+                        MuestraTarea();
+                        break;
+                    case 4:
+                        ModificaTarea();
+                        break;
+                    case 5:
+                        MarcaCompletada();
+                        break;
+                    case 6:
+                        Console.WriteLine("¡Hasta luego!");
+                        return;
+                    default:
+                        Console.WriteLine("Opción no válida: del 1 al 6.");
+                        break;
+                }
+            }
+        }
+
+        static void AddTarea()
+        {
+            Console.WriteLine("Introduzca el nombre de la tarea");
+            string nombreTarea = Console.ReadLine();
+            Console.WriteLine("Introduzca la descipcion de la tarea");
+            string descripcionTarea = Console.ReadLine();
+
+            Tarea nuevaTarea = new Tarea
+            {
+                nombre = nombreTarea,
+                descripcion = descripcionTarea,
+                completada = false
+            };
+
+            lista.Add(nuevaTarea);
+            Console.WriteLine("Tarea añadida a la lista.");
+        }
+
+        static void BorraTarea()
+        {
+            Console.WriteLine("Que tarea deseas borrar (Introduce el núm de tarea):");
+            int indice = int.Parse(Console.ReadLine());
+
+            lista.RemoveAt(indice - 1);
+            Console.WriteLine($"Tarea {indice} eliminada de la lista.");
+        }
+
+        static void MuestraTarea()
+        {
+            if (lista.Count == 0)
+            {
+                Console.WriteLine("No hay tareas.");
+                return;
+            }
+
+            Console.WriteLine("Lista de tareas:");
+            for (int i = 0; i < lista.Count; i++)
+            {
+                Console.WriteLine($"{i + 1}. {lista[i].nombre} - {lista[i].descripcion} " +         
+                $" - { (lista[i].completada ? "Completada" : "Pendiente")}");
+                // ? operador ternario si es true devuelve Completada si es false Pendiente
+            }
+        }
+
+        static void ModificaTarea()
+        {       
+            MuestraTarea();
+
+            Console.WriteLine("Que tarea deseas modificar (Introduce el núm de tarea):");
+            int indice = int.Parse(Console.ReadLine());
+
+            if (indice < 1 || indice > lista.Count)
+            {
+                Console.WriteLine("Índice fuera de rango. Por favor, introduce un número válido.");
+                return;
+            }
+
+            Tarea tareaModificar = lista[indice - 1];
+
+            Console.WriteLine("Introduzca el nombre de la tarea");
+            tareaModificar.nombre = Console.ReadLine();
+            Console.WriteLine("Introduzca la descipcion de la tarea");
+            tareaModificar.descripcion = Console.ReadLine();
+
+            lista[indice - 1] = tareaModificar;
+            Console.WriteLine("Tarea modificada correctamente.");
+        }
+
+        static void MarcaCompletada()
+        {
+            MuestraTarea();
+            Console.WriteLine("que tarea deseas marcar como hecha");
+            int indice = int.Parse(Console.ReadLine());
+
+            if (indice < 1 || indice > lista.Count)
+            {
+                Console.WriteLine("Índice fuera de rango. Por favor, introduce un número válido.");
+                return;
+            }
+
+            Tarea tareaHecha = lista[indice - 1];
+            tareaHecha.completada = true;
+            lista[indice - 1] = tareaHecha;
+            Console.WriteLine("Tarea marcada como hecha");
+        }
+    }
+}
---
+++ C#/wordle.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wordle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lista de palabras a adivinar
+            string[] palabras = { "mango", "perro", "cielo", "fuego", "roble", "delta", "fuerte",
+        "techo", "limón", "bruma", "ronda", "papel", "fruta" };
+
+
+            // Seleccionar aleatoriamente una palabra de la lista
+            string palabraSecreta = SeleccionarPalabraSecreta(palabras);
+
+            
+                // Mostrar mensaje de bienvenida
+                Console.WriteLine("¡Bienvenido a Wordle!");
+
+            do
+            {
+                Console.WriteLine("Adivina la palabra de 5 letras. Tienes 6 intentos.");
+
+                // Bucle principal del juego
+                for (int intento = 1; intento <= 6; intento++)
+                {
+                    // Obtener la palabra propuesta por el jugador
+                    string palabraPropuesta = Console.ReadLine().ToLower(); // Convertir a minúsculas para facilitar la comparación
+
+                    // Verificar y mostrar el resultado del intento
+                    MostrarResultado(palabraSecreta, palabraPropuesta);
+
+
+                    // Si la palabra es correcta, terminar el juego
+                    if (palabraPropuesta == palabraSecreta)
+                    {
+                        Console.WriteLine($"¡Felicidades! Has adivinado la palabra en {intento} intento/s.");
+
+                        int record = 6;
+
+                        if (intento < record)
+                        {
+                            Console.WriteLine("Enhorabuena has alcanzado un nuevo record");
+                        }
+
+                        return;
+                    }
+
+                }
+
+                // Mostrar la palabra correcta al finalizar los intentos
+                Console.WriteLine($"Lo siento, no has adivinado. La palabra correcta era: {palabraSecreta}");
+
+            } while (repetir());
+
+
+
+        }
+
+        // Función para seleccionar aleatoriamente una palabra de la lista
+        static string SeleccionarPalabraSecreta(string[] palabras)
+        {
+            Random random = new Random();
+
+            int indice = random.Next(palabras.Length);
+
+            return palabras[indice];
+
+        }
+
+        // Función para mostrar el resultado del intento
+        static void MostrarResultado(string palabraSecreta, string palabraPropuesta)
+        {
+            // Comparar letra por letra y mostrar el resultado en cada intento
+            for (int i = 0; i < palabraSecreta.Length; i++)
+            {
+                char letraSecreta = palabraSecreta[i];
+                char letraPropuesta = i < 5 ? palabraPropuesta[i] : '_'; //palabraPropuesta.Length
+
+
+                if (letraPropuesta == letraSecreta)
+                {
+                    Console.Write(char.ToUpper(letraPropuesta)); // Letra correcta y en la posición correcta
+
+                }
+                else if (palabraSecreta.Contains(letraPropuesta.ToString()))
+                {
+                    Console.Write(char.ToLower(letraPropuesta)); // Letra correcta pero en posición incorrecta
+
+                }
+                else
+                {
+                    Console.Write("_"); // Letra incorrecta
+
+                }
+
+            }
+
+            Console.WriteLine(); // Nueva línea para separar los resultados de cada intento
+        }
+
+        static bool repetir()
+        {
+            Console.WriteLine("Deseas volver a juagar (si 's' o no 'n')");
+            char respuesta = char.Parse(Console.ReadLine().ToLower());
+
+            if (respuesta == 's')
+            {
+                return true;
+            }
+            else 
+            { 
+                return false;
+            }
+
+        }
+    }
+}
